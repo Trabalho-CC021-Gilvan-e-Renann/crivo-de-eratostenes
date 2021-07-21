1b8b38d74837f694f8830d3f5a978107188ff3fa
Gilvan Gomes <42100457+GilvanG@users.noreply.github.com>
2021-07-21 13:23:58 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,164 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
-#include "lista.h"
 
 #define TRUE 1
 #define FALSE 0
 
-#define EROS TRUE
-//#define AKTIN TRUE
-#define PARALELO TRUE
-#define SERIAL TRUE
-//#define OUTPUT TRUE
 #define STA_TIME TRUE
+//#define OUTPUT TRUE
 
 int main(){
-  int n = 1000;
-  Lista *lista = (Lista*)malloc(sizeof(Lista));
-  lista->inicio = NULL;
-  lista->fim = NULL;
+  int n = 10000000;
+  int *num = malloc((1+n)*sizeof(int));
 
   int totalPrimos = n+1-2;
   #ifdef STA_TIME
-    double inicio, end, t_serial, t_paralelo;
+    double inicio, end, tiempo;
   #endif // STA_TIME
-  #ifdef EROS
-    int i, j;
+  int i, j;
+  printf("SERIAL\n");
+  printf("~>Iniciando Variaveis...\n");
+  for (i = 0; i <= n; i++) {
+    num[i]=i;
+  }
 
-    #ifdef SERIAL
-      printf("SERIAL\n");
-      printf("~>Iniciando Variaveis...\n");
-      for (i = 0; i <= n; i++) {
-        Num* numero = (Num*)malloc(sizeof(Num));
-        numero->num = i;
-        numero->marca = 0;
-        if(lista->fim == NULL && lista->inicio == NULL){
-          lista->inicio = numero;
-        }else{
-          lista->fim->prox = numero;
-        }
-        lista->fim = numero;
-        numero->prox = NULL;
+  printf("~>Variaveis Iniciaizadas...\n");
+  printf("~>Iniciando Buscas de Primos...\n");
+
+  #ifdef STA_TIME
+    inicio = omp_get_wtime();
+  #endif
+  num[0]=-1;
+  num[1]=-1;
+  for(int i = 2; i < n/2+1; i++){
+    for(int j=i; j <= n+1; j+=i){
+      if(i!=j && num[j]!=-1 && num[j]%i==0){
+        //printf("%d", i);
+        num[j] = -1;
+        totalPrimos--;
       }
 
-      printf("~>Variaveis Iniciaizadas...\n");
-      printf("~>Iniciando Buscas de Primos...\n");
-
-      #ifdef STA_TIME
-        inicio = omp_get_wtime();
-      #endif
-
-      Num* p = lista->inicio;
-      for(int i = 2; i <= n; i++){
-        while(p->prox != NULL){
-            if(p->prox->num % i == 0 && p->prox->num != i){
-                Num* atual = p->prox->prox;
-                p->prox->marca++;
-                p->prox = atual;
-            }
-            if(p->prox == NULL){
-                break;
-            }
-            p = p->prox;
-        }
-        p = lista->inicio;
-        if(p->num % i == 0 && p->num != i){
-           p->marca++;
-        }
+    }
+  }
+  #ifdef OUTPUT
+    for(int i = 2; i < n/2+1; i++){
+      if(num[j]!=-1){
+        printf("%d ", i);
       }
-      #ifdef STA_TIME
-        end = omp_get_wtime();
-        printf("~>***%f\n",inicio);
-        printf("~>***%f\n",end);
-        t_serial = end-inicio;
-        printf("~>Tempo para executar o crivo: %f\n", t_serial);
-      #endif
-
-      printf("~>Busca Concluida\n");
-      printf("~>%d Primos Encrontados", totalPrimos);
-
-      #ifdef OUTPUT
-        printf("\n");
-        for(i = 2; i <= n; i++){
-  	      if(primo[i]){
-            printf("%d ",i);
-          }
-        }
-      #endif
-    #endif // SERIAL
-    #ifdef PARALELO
-    printf("\n\n\n\n");
-
-    totalPrimos=n+1-2;
-    printf("PARALELO\n");
-      printf("~>Iniciando Variaveis...\n");
-      p = lista->inicio;
-      for(int i = 0; i <= n; i++){
-        if(p->prox == NULL){
-            break;
-        }
-        p->marca = 0;
-        p = p->prox;
-      }
-
-      printf("~>Variaveis Iniciaizadas...\n");
-      printf("~>Iniciando Buscas de Primos...\n");
-
-      #ifdef STA_TIME
-        inicio = omp_get_wtime();
-      #endif
-
-      #pragma omp parallel num_threads(4)
-      {
-        p = lista->inicio;
-        j=4;
-        #pragma omp for
-          for(int i = 2; i <= n; i++){
-            while(p->prox != NULL){
-              if(p->prox->num % i == 0 && p->prox->num != i){
-                Num* atual = p->prox->prox;
-                p->prox->marca++;
-                p->prox = atual;
-              }
-              if(p->prox == NULL){
-                break;
-              }
-              p = p->prox;
-            }
-            p = lista->inicio;
-            if(p->num % i == 0 && p->num != i){
-              p->marca++;
-              totalPrimos--;
-            }
-          }
-        }
-
-      #ifdef STA_TIME
-        end = omp_get_wtime();
-        printf("***%f\n",inicio);
-        printf("***%f\n",end);
-        t_paralelo = end-inicio;
-        printf("Tempo para executar o crivo: %f\n", t_paralelo);
-        double speedup = t_serial/t_paralelo;
-        printf("Speedup: %.4f\n", t_serial/t_paralelo);
-        printf("Eficiencia: %.4f\n",speedup/4.0);
-
-      #endif
-      printf("~>Busca Concluida\n");
-      printf("~>%d Primos Encontrados", totalPrimos);
-
-      #ifdef OUTPUT
-        printf("\n");
-        for(i = 2; i <= n; i++){
-  	      if(primo[i]){
-            printf("%d ",i);
-          }
-        }
-      #endif
-    #endif // PARALELO
+    }
+  #endif // OUTPUT
+  #ifdef STA_TIME
+    end = omp_get_wtime();
+    tiempo = end-inicio;
+    printf("~>Tempo Inicial: %f\n",inicio);
+    printf("~>Tempo Final: %f\n",end);
+    printf("~>Tempo: %f\n", tiempo);
   #endif
 
-
+  printf("~>Busca Concluida\n");
+  printf("~>%d Primos Encrontados", totalPrimos);
   return 0;
 }
